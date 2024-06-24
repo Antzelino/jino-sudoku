8f5fe7061dcc48255fe0e140c286dba080d1188f
Angelino <angelino_m@outlook.com>
2024-06-24 10:42:14 +0200

    cleanup in print_sudoku function

--- sudoku_util.h
+++ sudoku_util.h
@@ -8,14 +8,13 @@ void print_sudoku(unsigned short int board[9][9]) {
     {
         for (size_t j = 0; j < 9; j++)
         {
-            if(board[i][j] < 0 || board[i][j] > 9) {
+            if(board[i][j] > 9) {
                 fprintf(stderr, "INVALID: Sudoku contains invalid value \'%hu\' at [%zu][%zu]. Cannot print board.\n", board[i][j], i, j);
                 return;
             }
         }
     }
 
-    short printing_line = 1;
     for (size_t i = 0; i < 9; i++)
     {
         printf("+---+---+---+---+---+---+---+---+---+\n");
