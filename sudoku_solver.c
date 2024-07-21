3ea2689b7d81e2ea325d2efe01c166f89173cf11
Angelino <angelino_m@outlook.com>
2024-07-21 23:47:40 +0200

    implemented hidden singles, added new todo

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include "sudoku_solver.h"
 
+#define SUDOKU_MAX_ITERATIONS 10
+
 /*
  * Each element value from 1 to 9.
  * If 0 means that value is not possible in this cell.
@@ -71,7 +73,7 @@ void clear_number_from_column(unsigned short int n, unsigned short int i, unsign
     for (size_t i_ = 0; i_ < 9; i_ ++)
     {
         if (i_ == i)
-        {            
+        {
             assert(possible_values[i][j].values[n-1] != 0 && "Cell should have value, not 0");
             assert(possible_values[i][j].values[n-1] == n && "Cell has wrong value in index");
             continue;
@@ -115,6 +117,95 @@ int only_one_possible_value_in_cell(unsigned short int i, unsigned short int j) 
         }
     }
     return possible_value;
+}
+
+void check_hidden_singles() {
+    // First check for each value in each row:
+    for (unsigned short n = 1; n <= 9; n++)
+    {
+        for (unsigned short i = 0; i < 9; i++)
+        {
+            unsigned short row_n_count = 0;
+            short row_index = -1;
+
+            for (unsigned short j = 0; j < 9; j++)
+            {
+                if (possible_values[i][j].values[n-1] != 0)
+                {
+                    row_n_count++;
+                    row_index = j;
+                }
+            }
+            
+            if (row_n_count == 1)
+            {
+                assert(row_index >= 0 && row_index < 9 && "Row index not in valid range");
+                handle_number_in_cell(n, i, row_index);
+            }
+        }
+    }
+    // Then check for each value in each column
+    for (unsigned short n = 1; n <= 9; n++)
+    {
+        for (unsigned short j = 0; j < 9; j++)
+        {
+            unsigned short column_n_count = 0;
+            short column_index = -1;
+
+            for (unsigned short i = 0; i < 9; i++)
+            {
+                if (possible_values[i][j].values[n-1] != 0)
+                {
+                    column_n_count++;
+                    column_index = i;
+                }
+            }
+            
+            if (column_n_count == 1)
+            {
+                assert(column_index >= 0 && column_index < 9 && "Column index not in valid range");
+                handle_number_in_cell(n, column_index, j);
+            }
+        }
+    }
+    // Lastly check for each value in each 3x3 box
+    for (unsigned short n = 1; n <= 9; n++)
+    {
+        for (unsigned short box_center_i = 1; box_center_i < 9; box_center_i+=3)
+        {
+            for (unsigned short box_center_j = 1; box_center_j < 9; box_center_j+=3)
+            {
+                unsigned short box3x3_n_count = 0;
+                short n_index_i = -1;
+                short n_index_j = -1;
+
+                unsigned short int i_start, i_end, j_start, j_end;
+                i_start = (box_center_i / 3) * 3;
+                i_end = i_start + 2;
+                j_start = (box_center_j / 3) * 3;
+                j_end = j_start + 2;
+
+                // Iterating the 9 cells in this 3x3 box
+                for (unsigned short i_ = i_start; i_ <= i_end; i_++)
+                {
+                    for (unsigned short j_ = j_start; j_ <= j_end; j_++)
+                    {
+                        if (possible_values[i_][j_].values[n-1] != 0)
+                        {
+                            box3x3_n_count++;
+                            n_index_i = i_;
+                            n_index_j = j_;
+                        }
+                    }
+                }
+                if (box3x3_n_count == 1) {
+                    assert(n_index_i >= 0 && n_index_i < 9 && "Column index not in valid range");
+                    assert(n_index_j >= 0 && n_index_j < 9 && "Column index not in valid range");
+                    handle_number_in_cell(n, n_index_i, n_index_j);
+                }
+            }
+        }
+    }
 }
 
 /*
@@ -197,7 +288,7 @@ void solve_sudoku(unsigned short int input[9][9], unsigned short int output[9][9
         }
     }
 
-    for (size_t x = 0; x < 10; x++)
+    for (size_t x = 0; x < SUDOKU_MAX_ITERATIONS && is_not_fully_solved(output) == 0; x++)
     {
         for (size_t i = 0; i < 9; i++)
         {
@@ -212,6 +303,7 @@ void solve_sudoku(unsigned short int input[9][9], unsigned short int output[9][9
                 }
             }
         }
+        check_hidden_singles();
     }
 
     if(is_not_fully_solved(output) == 0) {
